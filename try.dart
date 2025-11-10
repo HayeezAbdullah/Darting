0377c4d6556d6a7ea28718145741ccc382beea59
Hayeez  Abdulllah <hayeezabdullah2k5@gmail.com>
2025-11-10 21:32:32 +0530

    calculator

--- try.dart
+++ try.dart
@@ -3,5 +3,17 @@ import 'dart:io';
 void main() {
   stdout.writeln('can you see JOHN CENA ?');
   String? answer = stdin.readLineSync();
-  print('yeah I can $answer');
+  print('$answer LIL BROO');
+
+  stdout.writeln('enter number : ');
+  int? number1 = int.tryParse(stdin.readLineSync()!);
+  stdout.writeln('enter number : ');
+  int? number2 = int.tryParse(stdin.readLineSync()!);
+
+  if (number1 != null && number2 != null) {
+    int sum = number1 + number2;
+    print('Answer : $sum');
+  } else {
+    print('Invalid Number BRO!');
+  }
 }
